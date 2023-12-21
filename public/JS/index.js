cdff5f4714f84029e4cb478b6818971e6418cb86
Rohanbasnet12 <rohanbmyname@gmail.com>
2023-12-20 22:42:58 -0800

    Initial commit

---
+++ public/JS/index.js
@@ -0,0 +1,8 @@
+// LOGIC FOR WEB LOADING USING EVENT LISTENER
+$(window).on('load', function () {
+    $('#loader').css({
+        'display': 'none',
+        'transition': 'all .3s'
+    });
+});
+
